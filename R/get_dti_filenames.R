9d115bfe58ab87caf0d5c2f6c818d632524e893b
muschellij2 <muschellij2@gmail.com>
2016-10-05 01:03:44 -0400

    adding files to repo

---
+++ R/get_dti_filenames.R
@@ -0,0 +1,15 @@
+#' @title Get DTI Image Filenames
+#'
+#' @description Return the filenames for the DTI images
+#' @param ... arguments to pass to \code{\link{get_image_filenames}},
+#' \code{modalities = "DTI"} so it cannot be specified
+#' @return Vector fo filenames
+#' 
+#' @examples
+#' get_dti_filenames()
+#' @export
+#' @importFrom kirby21.base get_image_filenames
+get_dti_filenames = function(...) {
+  x = kirby21.base::get_image_filenames(modalities = "DTI", ...)
+  return(x)
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+Structural DTI magnetic resonance imaging ('MRI')
+data from the 'Kirby21' reproducibility study.
+
+[![Travis-CI Build Status](https://travis-ci.org/muschellij2/kirby21.dti.svg?branch=master)](https://travis-ci.org/)
+
