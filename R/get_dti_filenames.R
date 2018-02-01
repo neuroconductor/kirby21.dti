8d3a1a024bcc5444325c17257bf93139828eb853
muschellij2 <muschellij2@gmail.com>
2018-02-01 11:06:51 -0500

    updated description with doi

--- R/get_dti_filenames.R
+++ R/get_dti_filenames.R
@@ -3,7 +3,7 @@
 #' @description Return the filenames for the DTI images
 #' @param ... arguments to pass to \code{\link{get_image_filenames}},
 #' \code{modalities = "DTI"} so it cannot be specified
-#' @return Vector fo filenames
+#' @return Vector of filenames
 #' 
 #' @examples
 #' get_dti_filenames()
